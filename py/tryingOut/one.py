1688d1de0638fb27bdb700292bd59833e34eea7b
Daniel <daniel.meneses@osaango.com>
2023-05-12 11:07:06 +0300

    playing iwth python fncs

--- py/tryingOut/one.py
+++ py/tryingOut/one.py
@@ -6,9 +6,15 @@ else:
   print("i is no longer less than 6") """ 
 
 
-Browsers = ["Chrome", "Firefox", "Edge", "Brave", "Opera"]  # INDEX IN ANY LANGUAGE STARTS FROM [0] -> [0..4]
+""" Browsers = ["Chrome", "Firefox", "Edge", "Brave", "Opera"]  # INDEX IN ANY LANGUAGE STARTS FROM [0] -> [0..4]
 for x in Browsers:
   if x == "Edge":
     #break
     print("Hey, I am the Edge browser")
-  #print(x)
+  #print(x) """
+
+def testWithBrowser(browser):
+    print(browser)
+
+#activeBrowser = testWithBrowser("Chrome")
+testWithBrowser("Firefox")
