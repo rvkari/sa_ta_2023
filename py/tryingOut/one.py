b1ed721e376ffc0676d630e78b38f50dc57c93e1
Daniel <daniel.meneses@osaango.com>
2023-05-12 11:48:07 +0300

    se2 & one.py update

--- py/tryingOut/one.py
+++ py/tryingOut/one.py
@@ -13,7 +13,7 @@ for x in Browsers:
     print("Hey, I am the Edge browser")
   #print(x) """
 
-def testWithBrowser(browser):
+""" def testWithBrowser(browser):
     print(browser)
 
 #activeBrowser = testWithBrowser("Chrome")
@@ -21,4 +21,18 @@ testWithBrowser("Firefox")
 
 a = 1
 
-b = "I am the number => "
+b = "I am the number => " """
+
+# ***** I am the function ****** #
+def check_correct_greeeting(greeting):
+    if greeting == "Hail Our Robot Overlords!":
+      print("You may proceed...")
+    else:
+       print("Sorry. But that was the wrong answer... Bye Bye...")
+# ***** function finishes here ****** #
+
+# I call the fnc for 1st time
+check_correct_greeeting("Hail Our Robot Overlords!")
+
+# I call the fnc for 2nd time
+check_correct_greeeting("Hello World!")
