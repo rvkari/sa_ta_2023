30e164e18faa880ab0d86555599569ae8e6e8657
Daniel <daniel.meneses@osaango.com>
2023-05-12 14:05:43 +0300

    finished 1_in.robot conversion into python syntax

--- py/tryingOut/two.py
+++ py/tryingOut/two.py
@@ -2,17 +2,49 @@
 mammals = ["cat", "dog", "cow"]
 birds = ["eagle", "falcon", "ladybird"]
 
+user1=    "peter"
+user2=    "bruce"
+user3=    "tony"
 
+### 1st test from line 11 on 1_in.robot file ###
 # My functions
 def loop_over_list(mammals):
-    print("Four mammals:")
+    print("3 mammals:")
     for var in mammals:
         print(var)
 
 # My program
-loop_over_list(mammals)
+#loop_over_list(mammals)    #uncomment this line to run that fnc
 
-
+### 2nd Test from line 17 on 1_in.robot file ###
 def Loop_over_two_lists_after_each_other(mammals, birds):
-    print("Four mammals & four birds:")
-    for var in mammals 
+    print("3 mammals & three birds:")
+    """ for var in mammals:
+        print(var)
+    for var in birds:
+        print(var) """
+    loop_over_list(mammals)
+    loop_over_list(birds)
+
+#Loop_over_two_lists_after_each_other(mammals, birds)   #uncomment this line to run that fnc
+
+
+
+### 3rd Test from line 23 on 1_in.robot file ###
+def loop_over_multiple_values(user1, user2, user3):
+    # ----- this is one way to implement the code described on 1_in.robot using it's syntax (RF)
+    listOfUsers = [user1, user2, user3, "Steve"]
+
+    for user in listOfUsers:
+        print(user)
+
+    # ----- this is another way to implement the code described on 1_in.robot using it's syntax (RF)
+    #print(user1, user2, user3, "Steve")
+
+    # ----- this is also another way to implement the code described on 1_in.robot using it's syntax (RF)
+    """ print(user1)
+    print(user2)
+    print(user3)
+    print("Steve") """
+
+loop_over_multiple_values(user1, user2, user3)
--- rf/forLoop/1_in.robot
+++ rf/forLoop/1_in.robot
@@ -9,19 +9,19 @@ ${user3}    tony
 
 *** Test Cases ***
 Loop over list
-    Log To Console    Four mammals:
+    Log To Console    3 mammals:
     FOR   ${var}    IN    @{mammals}
         Log To Console    ${var}
     END
 
 Loop over two lists after each other
-    Log To Console    Four mammals & four birds:
+    Log To Console    3 mammals & three birds:
     FOR   ${var}    IN    @{mammals}    @{birds}
         Log To Console    ${var}
     END
 
 Loop over multiple values
-    Log To Console    Four users:
+    Log To Console    3 users:
     FOR   ${var}    IN    ${user1}    ${user2}    ${user3}    Steve
         Log To Console    ${var}
     END
