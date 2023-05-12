98481d7e39f043858699e70a2882f1813e85821b
Daniel <daniel.meneses@osaango.com>
2023-05-12 10:56:40 +0300

    added for loop with browsers

--- py/tryingOut/one.py
+++ py/tryingOut/one.py
@@ -1,6 +1,14 @@
-i = 1
+""" i = 1
 while i < 6:
   print(i)
   i += 1
 else:
-  print("i is no longer less than 6") 
+  print("i is no longer less than 6") """ 
+
+
+Browsers = ["Chrome", "Firefox", "Edge", "Brave", "Opera"]  # INDEX IN ANY LANGUAGE STARTS FROM [0] -> [0..4]
+for x in Browsers:
+  if x == "Edge":
+    #break
+    print("Hey, I am the Edge browser")
+  #print(x)
